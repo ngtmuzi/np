5c6ed25f6dcd4034363cb2f79fe8d62d1228413f
xuezhongxiong <xuezhongxiong@henhaoji.com>
2017-01-18 17:01:52 +0800

    es6 class化，代码精简

--- index.js
+++ index.js
@@ -4,147 +4,102 @@
 'use strict';
 //0,1,2 : pending, fulfilled,rejected
 
-function Np(fn) {
-  this.state          = 0;
-  this.onFulfilledFns = [];
-  this.onRejectedFns  = [];
-  if (fn) {
-    try {
-      fn(this.resolve.bind(this), this.reject.bind(this));
-    } catch (err) {
-      this.state  = 2;
-      this.reason = err;
+class Np {
+
+  constructor(fn) {
+    this.state     = 0;
+    this.callbacks = [];
+
+    if (fn) {
+      try {
+        fn(this.resolve.bind(this), this.reject.bind(this));
+      } catch (err) {
+        this.state  = 2;
+        this.reason = err;
+      }
     }
   }
-}
 
-Np.prototype.resolve = function (value) {
-
-  setImmediate(() => {
-
+  //实现2.3 The Promise Resolution Procedure部分
+  resolve(value) {
     if (this.state) return;
     if (value === this) this.reject(new TypeError('same object'));
 
-    let hasBeenCalled = false;
-    if (value && (typeof value === 'object' || typeof value === 'function')) {
-      try {
-
-        let then = value.then;
-        if (typeof then === 'function')
-          return then.bind(value)(value => {
-            if (hasBeenCalled) return;
-            hasBeenCalled = true;
-            this.resolve(value);
-          }, reason => {
-            if (hasBeenCalled) return;
-            hasBeenCalled = true;
-            this.reject(reason);
-          });
-      } catch (err) {
-        if (hasBeenCalled) return;
-        hasBeenCalled = true;
-        return this.reject(err);
+    process.nextTick(() => {
+      if (value && (typeof value === 'object' || typeof value === 'function')) {
+        let isCalled = 0;
+        try {
+          let then = value.then;
+          if (typeof then === 'function')
+            return then.bind(value)(
+              value => !isCalled++ ? this.resolve(value) : 0,
+              reason => !isCalled++ ? this.reject(reason) : 0
+            );
+        } catch (err) {
+          if (!isCalled++) this.reject(err);
+          return;
+        }
       }
-    }
 
-    this.state = 1;
-    this.value = value;
-    this.onFulfilledFns.forEach(fn => fn(value));
-  });
-};
+      this.state = 1;
+      this.value = value;
+      this.callbacks.forEach(fn => fn(this));
+    });
+  }
 
-Np.prototype.reject = function (reason) {
-  setImmediate(() => {
+  reject(reason) {
     if (this.state) return;
     if (reason === this) this.reject(new TypeError('same object'));
-//    if (reason && typeof reason.then === 'function') {
-//      reason.then(value => this.reject(value), reason => this.reject(reason));
-//    } else {
-    this.state  = 2;
-    this.reason = reason;
-    this.onRejectedFns.forEach(fn => fn(reason));
-//    }
-  });
-};
-
-Np.prototype.then = function (onFulfilled, onRejected) {
+    process.nextTick(() => {
+      this.state  = 2;
+      this.reason = reason;
+      this.callbacks.forEach(fn => fn(this));
+    });
+  }
 
 
-//  if (typeof onFulfilled !=='function') throw new Error('axxx');
-//  if (arguments.length>1 && typeof onRejected!=='function') throw new Error('ssss');
+  then(onFulfilled, onRejected) {
 
-  const self = this;
-  if (this.state === 1) {
-    if (typeof onFulfilled === 'function')
-      return new Np((resolve, reject) => {
-        setImmediate(() => {
-          try {
-            resolve(onFulfilled(this.value))
-          } catch (err) {
-            reject(err);
+    if (this.state === 1) {
+      return (typeof onFulfilled === 'function') ?
+        Np.resolve({then: () => onFulfilled(this.value)}) :
+        this;
+    }
+    if (this.state === 2) {
+      return (typeof onRejected === 'function') ?
+        Np.resolve({then: () => onRejected(this.reason)}) :
+        this;
+    }
+
+    return new Np((resolve, reject) => {
+      this.callbacks.push((promise) => {
+        try {
+          if (this.state === 1) {
+            resolve(typeof onFulfilled === 'function' ?
+              onFulfilled(this.value) :
+              this.value);
+          } else {
+            if (typeof onRejected === 'function')
+              resolve(onRejected(this.reason));
+            else
+              reject(this.reason);
           }
-        });
+        } catch (e) {
+          reject(e);
+        }
       });
-    else
-      return Np.resolve(this.value);
-  }
-  if (this.state === 2) {
-    if (typeof onRejected === 'function')
-      return new Np((resolve, reject) => {
-        setImmediate(() => {
-          try {
-            resolve(onRejected(this.reason))
-          } catch (err) {
-            reject(err);
-          }
-        });
-      });
-    else
-      return Np.reject(this.reason);
-  }
-
-  return new Np(function (resolve, reject) {
-    self.onFulfilledFns.push(function (value) {
-      try {
-
-        typeof onFulfilled === 'function' ?
-          resolve(onFulfilled(value)) :
-          resolve(value);
-      } catch (err) {
-        reject(err);
-      }
-    });
-    self.onRejectedFns.push(function (reason) {
-      try {
-        typeof onRejected === 'function' ?
-          resolve(onRejected(reason)) :
-          reject(reason);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  });
-};
-
-Np.resolve = function (value) {
-  if (value && typeof value.then === 'function') {
-    return new Np(function (resolve, reject) {
-      value.then(resolve, reject);
     });
   }
+}
 
+
+Np.resolve = value => {
   const p = new Np();
   p.resolve(value);
   return p;
 };
 
-Np.reject = function (reason) {
-  if (reason && typeof reason.then === 'function') {
-    return new Np(function (resolve, reject) {
-      reason.then(reject, reject);
-    });
-  }
-
+Np.reject = reason => {
   const p = new Np();
   p.reject(reason);
   return p;
